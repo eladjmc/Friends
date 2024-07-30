1b79b08943fab54ad95cf4561879c81320177ee1
EladWebiks <elad_harel@webiks.com>
2024-07-30 12:38:58 +0300

    added details page and js file

--- Friends/Controllers/FriendsController.cs
+++ Friends/Controllers/FriendsController.cs
@@ -40,15 +40,28 @@ namespace Friends.Controllers
         public IActionResult Details(int? id)
         {
             if (id == null) return RedirectToAction("Index");
-            Friend? friend = Data.Get.Friends.Include(f=>f.Images).FirstOrDefault(f => f.Id == id);
+            Friend? friend = Data.Get.Friends.Include(f => f.Images).FirstOrDefault(f => f.Id == id);
             if (friend == null) return RedirectToAction("Index");
 
             return View(friend);
         }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult AddImage(int? id, IFormFile image)
+        {
+            if (id == null) return RedirectToAction("Index");
+            if (image == null) return RedirectToAction("Details", new { id = id });
+            Friend? friend = Data.Get.Friends.Include(f => f.Images).FirstOrDefault(f => f.Id == id);
+            if (friend == null) return RedirectToAction("Index");
+            friend.AddImage(image);
+            Data.Get.SaveChanges();
+
+            return RedirectToAction("Details", new {id=id});
+        }
         public IActionResult Delete(int? id)
         {
             if (id == null) return RedirectToAction("Index");
-            Friend? friendToDelete = Data.Get.Friends.Include((f) => f.Images).FirstOrDefault(f=>f.Id == id);
+            Friend? friendToDelete = Data.Get.Friends.Include((f) => f.Images).FirstOrDefault(f => f.Id == id);
             if (friendToDelete == null) return RedirectToAction("Index");
 
             Data.Get.Remove(friendToDelete);
---
+++ Friends/wwwroot/js/addImage.js
@@ -0,0 +1,11 @@
+﻿function addImage() {
+    const formElement = document.getElementById("add-image-form");
+    const addButtonElement = document.getElementById("add-image-btn");
+    if (formElement.style.display === "none") {
+        formElement.style.display = "block";
+        addButtonElement.innerHTML = "סגור"
+    } else {
+        formElement.style.display = "none";
+        addButtonElement.innerHTML = "פתח"
+    }
+}
